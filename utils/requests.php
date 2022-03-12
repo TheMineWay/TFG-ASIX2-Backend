c4be42282f5ae3dad2764a948a081af2318f4849
Joel Campos <joelcamposoliva9000@gmail.com>
2022-03-12 22:53:55 +0100

    Simple user retriever

--- utils/requests.php
+++ utils/requests.php
@@ -5,12 +5,18 @@
     $ip = $server['REMOTE_ADDR'];
     $post = json_decode(file_get_contents('php://input'), true);
     $data = $post["data"];
-  
-  
+
+    $user = false;
+    if(isset($post["auth"])) {
+      $user = fetchAuthUser($post, $ip);
+    }
+
     return [
       "ip"=>$ip,
       "post"=>$post,
-      "data"=>$data
+      "data"=>$data,
+      "user"=>$user ?? false,
+      "roles"=>$roles ?? []
     ];
   }
 
@@ -23,5 +29,22 @@
       ]
     );
     exit;
+  }
+
+  function fetchAuthUser($post, $ip) {
+    $session = select("activeSessions", ["where"=>"id = ".sanitize($post["auth"]["session"])]) ?? null;
+    
+    if(!$session) {
+      throwHttpError('404','sess'); // ❌: Session not found
+    }
+
+    if($session["ip"] != $ip) {
+      throwHttpError('401','sess'); // ❌: Session IP does not match
+    }
+
+    $uid = $session["user"];
+    $user = select("users", ["where"=>"id = \"$uid\"", "limit"=>1])[0] ?? false;
+
+    return $user;
   }
 ?>
