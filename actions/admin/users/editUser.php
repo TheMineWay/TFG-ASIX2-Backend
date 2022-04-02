495d0708abca67631656377f988e4176cf803258
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-02 15:04:52 +0200

    User editor

--- actions/admin/users/editUser.php
+++ actions/admin/users/editUser.php
@@ -8,8 +8,22 @@
     requirePermissions(['adminUsers'], $request);
     
     $uid = sanitize($data["userId"]);
+    $values = [
+        "name"=>validateLength($data["values"]["name"], ["min"=>1,"max"=>32]),
+        "lastName"=>validateLength($data["values"]["lastName"], ["min"=>1,"max"=>32]),
+        "email"=>isEmail($data["values"]["email"]),
+        "phone"=>isPhone($data["values"]["phone"]),
+        "login"=>validateLength($data["values"]["login"], ["min"=>6,"max"=>32]),
+        "birthdate"=>validateDate($data["values"]["birthdate"])
+    ];
+
+    $password = $data["values"]["password"] ?? null;
     
-    //update("users", "id = $uid", ["isBanned"=>'1']);
+    update("users", "id = $uid", $values);
+
+    if($password != null) {
+        update("users", "id = $uid", ["password"=>hashWithSalt(validatePassword($password))]);
+    }
     
     answer([]);
 
