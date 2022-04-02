7f677270db8e47bab93da834f3f5ba63226b81af
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-02 15:16:24 +0200

    Ban system

---
+++ actions/admin/users/unbanUser.php
@@ -0,0 +1,16 @@
+<?php
+    
+    include('../../../global.php');
+    
+    $request = request();
+    $data = $request["data"];
+    
+    requirePermissions(['adminUsers'], $request);
+    
+    $uid = sanitize($data["userId"]);
+    
+    update("users", "id = $uid", ["isBanned"=>'0']);
+    
+    answer([]);
+
+?>
