e139d10a63be46d856a4581a1cfc4aa97b2a3e8a
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-02 22:22:23 +0200

    Inventory update

---
+++ actions/admin/inventory/editItem.php
@@ -0,0 +1,25 @@
+<?php
+    include('../../../global.php');
+
+    $request = request();
+    $data = $request["data"];
+    $item = $data["item"];
+    
+    requirePermissions([
+        'adminInventory'
+    ], $request);
+
+    $id = sanitize($data["itemId"]);
+
+    $values = [
+        "name"=>validateLength($item["name"], ["min"=>2,"max"=>32]),
+        "description"=>validateLength($item["description"], ["min"=>2,"max"=>256]),
+        "discount"=>validateNumberRange($item["discount"], ["min"=>0,"max"=>100]),
+        "price"=>validateNumberRange($item["price"], ["min"=>0]),
+        "stock"=>validateNumberRange($item["stock"], ["min"=>0])
+    ];
+
+    update('inventory', "id = $id", $values);
+
+    answer([]);
+?>
