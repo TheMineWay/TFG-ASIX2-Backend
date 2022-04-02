682913450bfa90bc9f411d1d2bedd349c40e5ff7
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-02 22:23:28 +0200

    desc to 255

--- actions/admin/inventory/editItem.php
+++ actions/admin/inventory/editItem.php
@@ -13,7 +13,7 @@
 
     $values = [
         "name"=>validateLength($item["name"], ["min"=>2,"max"=>32]),
-        "description"=>validateLength($item["description"], ["min"=>2,"max"=>256]),
+        "description"=>validateLength($item["description"], ["min"=>2,"max"=>255]),
         "discount"=>validateNumberRange($item["discount"], ["min"=>0,"max"=>100]),
         "price"=>validateNumberRange($item["price"], ["min"=>0]),
         "stock"=>validateNumberRange($item["stock"], ["min"=>0])
