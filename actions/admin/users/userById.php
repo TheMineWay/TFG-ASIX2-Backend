2a31c2744547965527045ad81c0ef543eaaaded9
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-03 21:25:33 +0200

    Comment for future me

--- actions/admin/users/userById.php
+++ actions/admin/users/userById.php
@@ -5,7 +5,7 @@
     $request = request();
     $data = $request["data"];
     
-    requirePermissions(['adminUsers'], $request);
+    requirePermissions(['adminUsers'], $request); // Maybe implement multi permissions, because not only userAdmin may want to request
 
     $uid = sanitize($data["userId"]);
 
