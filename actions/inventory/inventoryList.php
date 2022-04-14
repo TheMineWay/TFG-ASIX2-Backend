500f67a0bfb1b2a42d9ab2b1580910e1d8976efa
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-14 15:25:45 +0200

    isDrive in user inventory

--- actions/inventory/inventoryList.php
+++ actions/inventory/inventoryList.php
@@ -5,7 +5,7 @@
     requireAuth($request);
 
     $inventory = array_map(function ($inv) {
-        return lodash($inv, ["id","name","price","stock","discount","description","imageUrl"]);
+        return lodash($inv, ["id","name","price","stock","discount","description","imageUrl","isDrive"]);
     }, select('inventory')["data"]);
 
     answer([
