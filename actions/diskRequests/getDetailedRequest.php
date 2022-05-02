2e782503f6240f27c97f4a1f78bcb68343f0c11e
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-02 22:10:54 +0200

    Retrieve states

--- actions/diskRequests/getDetailedRequest.php
+++ actions/diskRequests/getDetailedRequest.php
@@ -9,10 +9,14 @@
     $purchaseId = $request["data"]["id"];
 
     $purchase = select("detailedPurchases", ["where"=>"user = '$userId'"])["data"][0];
+    $purchaseId = $purchase["id"];
+    $states = select("purchaseState", ["where"=>"purchase = '$purchaseId'"])["data"];
 
     answer([
         "purchase"=>lodash($purchase, ["id", "user", "createdAt", "address", "state", "amount"]),
-        "states"=>[], // Object
+        "states"=>array_map(function ($row) {
+            return lodash($row, ["id", "createdAt", "comment", "state"]);
+        }, $states), // Object
         "payment"=>[], // Object
         "builds"=>[], // Array
     ]);
