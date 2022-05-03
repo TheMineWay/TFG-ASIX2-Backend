e728256dc01515d0b06f15889e4ed64e19a860e2
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-03 21:38:59 +0200

    Retrieve build

--- actions/diskRequests/getDetailedRequest.php
+++ actions/diskRequests/getDetailedRequest.php
@@ -8,13 +8,22 @@
 
     $purchaseId = $request["data"]["id"];
 
+    // Get purchase
     $purchase = select("detailedPurchases", ["where"=>"user = '$userId'"])["data"][0];
     $purchaseId = $purchase["id"];
 
+    // Get states
     $states = select("purchaseState", ["where"=>"purchase = '$purchaseId'", "paranoid"=>false])["data"];
 
+    // Get payment
     $paymentId = $purchase["payment"];
     $payment = select("payments", ["where"=>"id = '$paymentId'"])["data"][0];
+
+    // Get builds
+    $builds = select("purchaseBuild", [
+        "where"=>"purchase = '$purchaseId'",
+        "fields"=>["id", "amount", "disk"
+    ]])["data"];
 
     answer([
         "purchase"=>lodash($purchase, ["id", "user", "createdAt", "address", "state", "amount"]),
@@ -22,6 +31,12 @@
             return lodash($row, ["id", "createdAt", "comment", "state"]);
         }, $states), // Object
         "payment"=>$payment, // Object
-        "builds"=>[], // Array
+        "builds"=>array_map(function ($row) {
+            $buildId = $row["id"];
+            return [
+                "build"=>$row,
+                "items"=>select("purchaseItems", ["where"=>"build = '$buildId'", "fields"=>["id"]])["data"]
+            ];
+        }, $builds), // Array
     ]);
 ?>
