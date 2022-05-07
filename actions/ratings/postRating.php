70e0eb143c008210cee60245fedc4bf2cffc6ee0
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-07 18:46:05 +0200

    is public added

--- actions/ratings/postRating.php
+++ actions/ratings/postRating.php
@@ -8,13 +8,14 @@
     
     $opinion = validateLength($request["data"]["opinion"], ["min"=>0,"max"=>500]);
     $rating = validateNumberRange($request["data"]["score"], ["min"=>1,"max"=>5]);
+    $isPublic = $request["data"]["isPublic"];
 
     $sanRating = sanitize($rating);
     $sanOpinion = sanitize($opinion);
 
     delete("opinions", "user = '$uid'");
     select("opinions", ["where"=>"rating=$sanRating AND opinion=$sanOpinion"])["data"];
-    insert("opinions", [[uuid("opinions"), $uid, $rating, $opinion]], ["id", "user", "rating", "opinion"]);
+    insert("opinions", [[uuid("opinions"), $uid, $rating, $opinion, $isPublic]], ["id", "user", "rating", "opinion", "isPublic"]);
 
     answer([]);
 ?>
