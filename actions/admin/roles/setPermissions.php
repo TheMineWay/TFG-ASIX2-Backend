96ebf3413a9902ef3fc60777d3f9b823baa7e098
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-20 12:42:15 +0200

    Modify perms

---
+++ actions/admin/roles/setPermissions.php
@@ -0,0 +1,42 @@
+<?php
+    include('../../../global.php');
+
+    $request = request();
+    
+    requirePermissions([
+        'adminRoles'
+    ], $request);
+
+    $role = $request["data"]["role"];
+    $roleId = sanitize($request["data"]["role"]);
+    $permissions = array_map(function ($perm) {
+        return sanitize($perm);
+    }, $request["data"]["permissions"]);
+
+    $currentPerms = array_map(function ($perm) {
+        return $perm["permission"];
+    }, select("permissionsAssign", ["where"=>"role = $roleId"])["data"]);
+    $permissionsToAdd = [];
+
+    foreach($request["data"]["permissions"] as $perm) {
+        if(in_array($perm, $currentPerms)) {
+            continue;
+        }
+        $permissionsToAdd[] = $perm;
+    }
+
+    if(count($permissionsToAdd) > 0) {
+        
+        $insertValues = [];
+        foreach($permissionsToAdd as $perm) {
+            $insertValues[] = [uuid("permissionsAssign"), $role, $perm];
+        }
+        
+        insert('permissionsAssign', $insertValues, ["id","role","permission"]);
+    }
+
+    $permissions = implode(",", $permissions);
+    delete('permissionsAssign', "`role` = $roleId AND permission NOT IN ($permissions)");
+
+    answer([]);
+?>
