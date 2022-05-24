541ede128984eaa689df83be8b6158491f73a4aa
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-24 16:47:57 +0200

    Message

--- utils/sendgrid.php
+++ utils/sendgrid.php
@@ -47,4 +47,21 @@
 
         }
     }
+
+    function registerMail($sendTo) {
+        $subject = "Et donem la benvinguda a Plugandwork! 🤠";
+        $content = '
+            <div>
+            <h2 style="text-align: justify;"><strong>Et donem la benvinguda a Plugandwork!</strong></h2>
+            <p style="text-align: justify;">T\'enviem aquest correu per donar-te la benvinguda a plugandwork.cat. Ara ja pots demanar discs personalitzats a trav&eacute;s de la nostra web.</p>
+            <p style="text-align: justify;">&nbsp;</p>
+            <p style="text-align: center;"><iframe class="giphy-embed" src="https://giphy.com/embed/XD9o33QG9BoMis7iM4" width="480" height="270" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
+            <p>&nbsp;</p>
+            <hr />
+            <p><strong>❗ AV&Iacute;S ❗</strong></p>
+            <p>Aquest missatge forma part del projecte de final de grau (<a href="https://plugandwork.cat">plugandwork.cat</a>). No oferim un servei real. No enviem missatges publicitaris, es probable que no rebis correus d\'aquest tipus mai m&eacute;s.</p>
+            </div>
+        ';
+        sendgridMail($sendTo, $subject, $content);
+    }
 ?>
