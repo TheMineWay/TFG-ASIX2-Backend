c2bb126b5064f33471960db1f7e39860569b8d29
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-24 17:25:14 +0200

    Send mail

--- utils/sendgrid.php
+++ utils/sendgrid.php
@@ -4,6 +4,8 @@
         try {
             $curl = curl_init();
 
+            $subject = str_replace("\n", "", $subject);
+
             curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://api.sendgrid.com/v3/mail/send',
             CURLOPT_RETURNTRANSFER => true,
@@ -48,20 +50,10 @@
         }
     }
 
-    function registerMail($sendTo) {
+    function registerMail($sendTo, $name) {
         $subject = "Et donem la benvinguda a Plugandwork! 🤠";
-        $content = '
-            <div>
-            <h2 style="text-align: justify;"><strong>Et donem la benvinguda a Plugandwork!</strong></h2>
-            <p style="text-align: justify;">T\'enviem aquest correu per donar-te la benvinguda a plugandwork.cat. Ara ja pots demanar discs personalitzats a trav&eacute;s de la nostra web.</p>
-            <p style="text-align: justify;">&nbsp;</p>
-            <p style="text-align: center;"><iframe class="giphy-embed" src="https://giphy.com/embed/XD9o33QG9BoMis7iM4" width="480" height="270" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
-            <p>&nbsp;</p>
-            <hr />
-            <p><strong>❗ AV&Iacute;S ❗</strong></p>
-            <p>Aquest missatge forma part del projecte de final de grau (<a href="https://plugandwork.cat">plugandwork.cat</a>). No oferim un servei real. No enviem missatges publicitaris, es probable que no rebis correus d\'aquest tipus mai m&eacute;s.</p>
-            </div>
-        ';
+        $content = '<div><h2 style=\"text-align: justify;\"><strong>'.$name.', et donem la benvinguda a Plugandwork!</strong></h2><p style=\"text-align: justify;\">T\'enviem aquest correu per donar-te la benvinguda a plugandwork.cat. Ara ja pots demanar discs personalitzats a trav&eacute;s de la nostra web.</p><p style=\"text-align: justify;\">&nbsp;</p><p style=\"text-align: center;\"><iframe class=\"giphy-embed\" src=\"https://giphy.com/embed/XD9o33QG9BoMis7iM4\" width=\"480\" height=\"270\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe></p><p>&nbsp;</p><hr /><p><strong>❗ AV&Iacute;S ❗</strong></p><p>Aquest missatge forma part del projecte de final de grau (<a href=\"https://plugandwork.cat\">plugandwork.cat</a>). No oferim un servei real. No enviem missatges publicitaris, es probable que no rebis correus d\'aquest tipus mai m&eacute;s.</p></div>';
+        echo $content;
         sendgridMail($sendTo, $subject, $content);
     }
 ?>
